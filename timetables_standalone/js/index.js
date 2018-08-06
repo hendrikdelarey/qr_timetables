d5efec97ba371a8a431fb569baf472730d193dfe
hendrikdelarey <vanjaarsveld.henk@gmail.com>
2018-08-06 13:16:53 +0200

    Added standalone_timetables

---
+++ timetables_standalone/js/index.js
@@ -0,0 +1,89 @@
+function checkTime(i) {
+        if (i < 10) {
+                i = "0" + i
+        };
+        return i;
+}
+
+function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('time').innerHTML =h + ":" + m;
+        var t = setTimeout(startTime, 500);
+}
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+function toDateString(date) {
+        return date.toTimeString().split(' ')[0].slice(0, -3); 
+}
+
+function showErrorMessage(error) {
+        document.getElementById('log').innerHTML = 'Error loading timetables';    
+}
+
+function bindStopNameToPage(stop) {
+        document.getElementById('title').innerHTML = stop.name;
+        document.getElementById('agency').innerHTML = stop.agency.name;
+}
+
+function bindStopTimeTableToPage(timetable) {
+        let timetableBody = '';
+        
+        timetable.forEach((item, index, arr) => {
+                timetableBody += `<tr> 
+                <td>${item.line.shortName}</td>
+                <td>${item.vehicle.headsign}</td>
+                <td>${toDateString(new Date(item.arrivalTime))}</td>
+                <td>${toDateString(new Date(item.departureTime))}</td>
+                </tr>`
+        });
+
+        document.getElementById('timetable-body').innerHTML = timetableBody;
+}
+
+function main() {
+        startTime();
+        let stopId = getQueryVariable('stopid');
+        let lineId = getQueryVariable('lineid');
+        let clientKey = getQueryVariable('key');
+        let clientSecret = getQueryVariable('secret');
+
+        if(stopId) {
+                getStop(stopId)
+                .then(stop => bindStopNameToPage(clientKey, clientSecret, stop))
+                .catch(error => {
+                        showErrorMessage(error);
+                });
+
+                getStopTimetable(stopId)
+                .then(timetable => {
+                        bindStopTimeTableToPage(timetable);
+                 })
+                 .catch(error => {
+                        showErrorMessage(error);
+                 });   
+        }
+        else if(lineId) {
+                getLineTimetable(clientKey, clientSecret, lineId)
+                .then(timetable => bindLineTimetableToPage(timetable))
+                .catch(error => {
+                        showErrorMessage(error);
+                })
+        }
+}
+
+main();
---
+++ timetables_standalone/js/tapi.js
@@ -0,0 +1,113 @@
+var getBearerToken = function(key, secret) { 
+    var payload = {
+        'client_id': key,
+        'client_secret': secret,
+        'grant_type': 'client_credentials',
+        'scope': 'transportapi:all'
+      };
+    
+    return new Promise(
+        (resolve, reject) => {
+            if(window.token_expires_in != false && window.token_expires_in > new Date().getTime()) {
+                resolve (window.token);
+            }
+            else {
+                var request = new XMLHttpRequest();
+                request.open('POST', 'https://identity.whereismytransport.com/connect/token', true);
+                request.addEventListener('load', function () {
+                    var response = JSON.parse(this.responseText);
+                    var token = response.access_token;
+                    window.token = token;
+                    window.token_expires_in = new Date().getTime() + response.expires_in;
+                    resolve(token);
+                });
+                request.addEventListener("error", error => reject({message:'error', err: error }));
+                request.addEventListener("abort", error => reject({message:'abort', err: error }));
+
+                request.setRequestHeader('Accept', 'application/json');
+                var formData = new FormData();
+                for (var key in payload) {
+                    formData.append(key, payload[key]);
+                }
+                request.send(formData);
+            }
+        }
+    )};
+
+var getStopFromTapi = function(token, stopId) {
+    return new Promise(
+        (resolve, reject) => {
+            var request = new XMLHttpRequest();
+            request.open('GET', 'https://platform.whereismytransport.com/api/stops/' + stopId, true);
+            request.setRequestHeader('Content-type', 'application/json');
+            request.setRequestHeader('Authorization', 'Bearer ' + token);
+            request.addEventListener('load', function () {
+                resolve(JSON.parse(this.responseText));
+            });
+            request.addEventListener("error", error => reject({message:'error', err: error }));
+            request.addEventListener("abort", error => reject({message:'abort', err: error }));
+            request.send();
+        }
+    );
+};
+
+var getStopTimetables = function(token, stopId) {
+    return new Promise(
+        (resolve, reject) => {
+            var request = new XMLHttpRequest();
+            request.open('GET', 'https://platform.whereismytransport.com/api/stops/' + stopId + '/timetables', true);
+            request.setRequestHeader('Content-type', 'application/json');
+            request.setRequestHeader('Authorization', 'Bearer ' + token);
+            request.addEventListener('load', function () {
+                resolve(JSON.parse(this.responseText));
+            });
+            request.addEventListener("error", error => reject({message:'error', err: error }));
+            request.addEventListener("abort", error => reject({message:'abort', err: error }));
+            request.send();
+        }
+    );
+};
+
+var getLineTimetables = function(token, lineId) {
+    return new Promise(
+        (resolve, reject) => {
+            var request = new XMLHttpRequest();
+            request.open('GET', 'https://platform.whereismytransport.com/api/lines/' + lineId + '/timetables', true);
+            request.setRequestHeader('Content-type', 'application/json');
+            request.setRequestHeader('Authorization', 'Bearer ' + token);
+            request.addEventListener('load', function () {
+                resolve(JSON.parse(this.responseText));
+            });
+            request.addEventListener("error", error => reject({message:'error', err: error }));
+            request.addEventListener("abort", error => reject({message:'abort', err: error }));
+            request.send();
+        }
+    );
+};
+
+async function getStop(key, secret, stopId) {
+    return new Promise((resolve,reject) => {
+        getBearerToken(key, secret)
+        .then(token => getStopFromTapi(token, stopId)) 
+        .then(fulfilled => resolve(fulfilled))
+        .catch(error => reject(error.message));
+    })
+}
+
+async function getStopTimetable(key, secret, stopId) {
+    return new Promise((resolve,reject) => {
+        getBearerToken(key, secret)
+        .then(token => getStopTimetables(token, stopId)) 
+        .then(fulfilled => resolve(fulfilled))
+        .catch(error => reject(error.message));
+    })
+}
+
+async function getLineTimetable(key, secret, lineId) {
+    return new Promise((resolve,reject) => {
+        getBearerToken(key, secret)
+        .then(token => getLineTimetables(token, lineId)) 
+        .then(fulfilled => resolve(fulfilled))
+        .catch(error => reject(error.message));
+    })
+}
