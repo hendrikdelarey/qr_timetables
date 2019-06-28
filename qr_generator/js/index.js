84254f15b2c5e65e7c8b0ccaec175477edb651bb
Henk <henk@sensornetworks.co.za>
2019-06-28 14:15:41 +0200

    Made qr code generator more generic

--- qr_generator/js/index.js
+++ qr_generator/js/index.js
@@ -1,67 +1,23 @@
-let agencies = [];
-let stops = [];
-let selectedStop = null;
-
-function setupStopsList() {
-    let stopSelectBody = '<option value="0">Select Stop</option>';
-    stops.forEach((value, index, arr) => {
-        stopSelectBody += `
-            <option value='${value.id}'>${value.name}</option>`;
-    })
-
-    document.getElementById('stopSelect').innerHTML = stopSelectBody;
-}
-
-function setupAgencyList() {
-    let agencySelectBody = '<option value="0">Select Agency</option>';
-    agencies.forEach((value, index, arr) => {
-        agencySelectBody += `
-            <option value='${value.id}'>${value.name}</option>`;
-    })
-
-    document.getElementById('agencySelect').innerHTML = agencySelectBody;
-}
-
-function onAgencySelectedChanged () {
-    var e = document.getElementById("agencySelect");
-    var value = e.options[e.selectedIndex].value;
-
-    if(value == "0") {
-        document.getElementById('stopSelect').innerHTML = '';
-    }
-    else {
-        getStops(value)
-        .then(result => {
-            stops = result;
-            setupStopsList();
-        })
-        .catch(error => console.log(error));
-    }
-}
 
 function displayQrCode(qrCode) { 
     console.log('Display QR code');
     let imageView = document.getElementById('qrCodeImageView');
-    document.getElementById('qrCodeImageView').innerHTML = `
+    document.getElementById('qrCodeImageView').innerHTML += `<span>${qrCode}</span><br/>
     <iframe width=500 height=500 src='${qrCode}'></iframe>
     `
     console.log("finish");
 }
 
 function onGenerateQrCodeButtonClick () {
-    var e = document.getElementById("stopSelect");
-    var value = e.options[e.selectedIndex].value;
+    var e = document.getElementById("text_input");
+    var qrCodeData = e.value.split(',');
 
-    displayQrCode(getQrCodeUrl(value));   
+    qrCodeData.forEach(qr => {
+        displayQrCode(getQrCodeUrl(qr)); 
+    });
 }
 
 function main() {
-    getAgencies()
-    .then(result => {
-        agencies = result;
-        setupAgencyList();
-    })
-    .catch(error => console.log(error));
 }
 
 main();
--- qr_generator/js/qrCodeGenerator.js
+++ qr_generator/js/qrCodeGenerator.js
@@ -1,9 +1,8 @@
 const url = "https://api.qrserver.com/v1/create-qr-code/";
 const size = 480;
-const destinationUrl = 'https://hendrikdelarey.github.io/qr_timetables/timetables/index.html?stopid=';
 
 function getQrCodeUrl (value) {
-    let target = destinationUrl + value;
+    let target = value;
     target = encodeURI(target);
     let rawUrl = `${url}?size=${size}x${size}&data=${target}`;
     return encodeURI(rawUrl);
